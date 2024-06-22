2ad6202b5f2dc4544294f90b4f2532b1cf595191
Tran Quan <quan.tm.1954@aptechlearning.edu.vn>
2024-06-22 13:30:30 +0700

    th

---
+++ assets/Farmwork/circle-loader/js/script.js
@@ -0,0 +1,12 @@
+﻿// JavaScript to show the loader when content is loading
+function showLoader() {
+    $("#overlay").fadeIn(300);
+}
+
+// JavaScript to hide the loader when content is loaded
+function hideLoader() {
+    setTimeout(function () {
+        $("#overlay").fadeOut(300);
+    }, 500);
+}
+
